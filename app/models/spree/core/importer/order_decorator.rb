556f610f592c58b7523beb6bf99ccebcca04bfe9
Patrick Sinclair <metade@gmail.com>
2014-08-01 12:05:51 +0100

    Raising an error if the line_item failed to save

---
+++ app/models/spree/core/importer/order_decorator.rb
@@ -0,0 +1,25 @@
+module Spree
+  module Core
+    module Importer
+      Order.class_eval do
+        def self.create_line_items_from_params(line_items_hash, order)
+          return {} unless line_items_hash
+          line_items_hash.each_key do |k|
+            begin
+              line_item = line_items_hash[k]
+              ensure_variant_id_from_params(line_item)
+
+              extra_params = line_item.except(:variant_id, :quantity, :options)
+              line_item = order.contents.add(Spree::Variant.find(line_item[:variant_id]), line_item[:quantity], line_item[:options])
+              line_item.save!
+              raise "Invalid line item: #{line_item}" unless line_item.valid?
+              line_item.update!(extra_params) unless extra_params.empty?
+            rescue Exception => e
+              raise "Order import line items: #{e.message} #{line_item}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
