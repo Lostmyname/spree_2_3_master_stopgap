f8b66df049e78b2580e21280821f27ae1c38879f
Vitor Arimitsu <to@vitork.com>
2015-02-17 17:45:24 -0200

    Overwriting method to create inventory units if they don't exist

--- app/models/spree/core/importer/order_decorator.rb
+++ app/models/spree/core/importer/order_decorator.rb
@@ -53,6 +53,64 @@ module Spree
             end
           end
         end
+
+        def self.create_shipments_from_params(shipments_hash, order)
+          return [] unless shipments_hash
+
+          line_items = order.line_items
+          shipments_hash.each do |s|
+            begin
+              shipment = order.shipments.build
+              shipment.tracking       = s[:tracking]
+              shipment.stock_location = Spree::StockLocation.find_by_admin_name(s[:stock_location]) || Spree::StockLocation.find_by_name!(s[:stock_location])
+
+              inventory_units = s[:inventory_units] || []
+              inventory_units.each do |iu|
+                ensure_variant_id_from_params(iu)
+
+                unit = shipment.inventory_units.build
+                unit.order = order
+
+                # Spree expects a Inventory Unit to always reference a line
+                # item and variant otherwise users might get exceptions when
+                # trying to view these units. Note the Importer might not be
+                # able to find the line item if line_item.variant_id |= iu.variant_id
+                unit.variant_id = iu[:variant_id]
+                unit.line_item_id = line_items.select do |l|
+                  l.variant_id.to_i == iu[:variant_id].to_i
+                end.first.try(:id)
+              end
+
+              # Creating inventory units if they don't exist
+              if shipment.save! && shipment.inventory_units.empty?
+                shipment.order.line_items.each do |line_item|
+                  line_item.target_shipment = shipment
+                  line_item.save
+                end
+              end
+
+              # Mark shipped if it should be.
+              if s[:shipped_at].present?
+                shipment.shipped_at = s[:shipped_at]
+                shipment.state      = 'shipped'
+                shipment.inventory_units.each do |unit|
+                  unit.state = 'shipped'
+                end
+              end
+
+              shipment.save!
+
+              shipping_method = Spree::ShippingMethod.find_by_name(s[:shipping_method]) || Spree::ShippingMethod.find_by_admin_name!(s[:shipping_method])
+              rate = shipment.shipping_rates.create!(:shipping_method => shipping_method,
+                                                     :cost => s[:cost])
+              shipment.selected_shipping_rate_id = rate.id
+              shipment.update_amounts
+
+            rescue Exception => e
+              raise "Order import shipments: #{e.message} #{s}"
+            end
+          end
+        end
       end
     end
   end
